3b04b89e8f72eb01a10a9c513c0ae037744d2259
afc51576 <afc51576@tuofs.com>
2019-09-04 15:39:03 +0200

    Modification of Tab 1 and 3, variables.scss (background-color); Enabling of configuration.service.ts for language choice in Settings (Tab 3)

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { i18n } from '../i18n';
+import {ConfigurationService} from '../configuration.service';
+
+
 
 @Component({
   selector: 'app-tab1',
@@ -9,14 +12,32 @@ import { i18n } from '../i18n';
 export class Tab1Page {
 
   public dict: Record<string, Record<string, string>> = {
-    de: {pageTitle: 'Übersicht'},
-    en: {pageTitle: 'Overview'},
+    de: {
+      pageTitle: 'Dein Führer',
+      pageMorning:'Guten Morgen, ich bin heute dein Führer',
+      pageAfternoon: 'Guten Tag, ich bin heute dein Führer',
+      pageEvening: 'Guten Abend, ich bin heute dein Führer',
+      pageSubtitle: 'Noch keinen Plan für heute Abend? Dann machen wir uns jetzt einen!',
+    },
+    en: {
+      pageTitle: 'Your Guide, tonight',
+      pageMorning: 'Good morning, I am your guide',
+      pageAfternoon: 'Hello, I am your guide',
+      pageEvening: 'Hey, I am drunk',
+      pageSubtitle: 'Have not got a plan yet? Lets do one!',
+    },
   };
 
-  public lang = 'en';
-  constructor() {}
+  constructor(
+      public configurationService: ConfigurationService
+  ) {}
 
   public i18n(key, fallback = '') {
-    return i18n(this.dict)(this.lang, key, fallback);
+    return i18n(this.dict)(this.configurationService.language, key, fallback);
+  }
+
+  public getHours() {
+    const now = new Date();
+    return now.getHours();
   }
 }
